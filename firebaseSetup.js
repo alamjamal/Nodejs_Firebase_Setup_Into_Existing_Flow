285edc864634b7e67ff3bc4925e076b85b326fd0
ALAM  JAMAL <alamjamal88@gmail.com>
2024-06-05 14:05:43 +0530

    Update firebaseSetup.js

--- firebaseSetup.js
+++ firebaseSetup.js
@@ -2,12 +2,12 @@ module.exports = {
     "type": "service_account",
     "project_id": "",
     "private_key_id": "",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDxXuu6L+5zKqd5\n1CXooI79QMhv6KXhknRCNEAqTHVxx5uPBSCdC5TU4ZoQ8LY9xwUFEYEBk+frvSlt\nZoHHhYhyKgERhzld0mJP6SK7srl0HUjlGW1e6havB+zPRbCisXwDjnyAZOv7ySha\nruM8qfpnrMPZDtkx6OK8GHNzHKbITHt4trPKZFpNOGBgaIFFqWpV58NXxLI+j0pd\nwcGkVInqrCkawZkRhUYI+6Jbnp0OlcsGI0Dz/8Hb+zPUQertR2aulJc9B225rT8U\n3k1eS2ccX/C92ajnM042nhA6d7UDwljV6e586rDFm4GpGXRYEUe0oN7EZ3d6Ull4\n+qsXdTH3AgMBAAECggEAJCI0odvZE46YIcnbnvaiUVFocDBg3OMC2oJd9LcbgdwC\n8qcu2bPJJRFc8hpG/fqUSszzBhxxRL49vG3zEb5wzb5yCIC42s3K//GDzJuV3ccg\niYOD9wiOOsex8Nma9tAhI5sqEkKJfVkA3Ip/5C4CGZEohI8PfsRBZS0340yvvx5B\nk9grg6HDX/IK+Yp+IVDfCgw4A16qqGg+iPeZJ80pBPpIOorjGpeYzUDiG/YIHacv\nCGi8BO9gaOnhHCPfcy5uqOHvirQQJvlzeVg59OPi67EmNE6X0TvdyvLBpKGW3VtE\nsx/A8zwQTgLGzQn351DzebJW/50E5sfpEfZaZcI5gQKBgQD6ywt0i4kxVnEo653j\nI27BmRCLlz44+o0q8gLPW+BDSYY0dZKndvIAR2poU02+VNRKOCGHHIUT2oc8XWJe\nX6YBqZFAVEeL7XkR4BCVu/rI040t+GcHjyKEUZ+EeorSDcsmCejTKYOZW+w4wVjI\nNxK7mBMWMQOezSYAfApbFUN8wQKBgQD2Ycvus3w7oNEA7q/AFf3M9QM1kiqFSuzv\nxO3Hyv1zywvHYND3Ji523gsL01fVu1BLCY6yNH2ykHz32vDitiFOs6OXtTnRsnx+\nwLJj0dxaI9+goShQTZxIMLPMtikSD3SFvJ+lmABSkGejS/uDkpuU1foGoGJLIFPH\nPCbCc0QEtwKBgQDKxXhRlrAqD/YWW8qgZ3mZViSmeHZ6ozmYS2wyM9qzDLNRnu4g\nMG+iAhvqlc/536R1DcUt9MU0NC+7gtWR8EvMdElYdcwTOsH0zfEa34346fLwBDNp\nTTVfPo93EY1EIJrr+n8gnCQmzsfDa7BY0Q54VpC4FJEUyFG9eoz079BNAQKBgQDE\nz2Q2/jsL6q9pvkC4ARknIsT4gxTiOvK/qSCEC1ugpp53GphyBJbzLvXF8YHlYmaG\nOlE5ewK5o/+fgc4QW4qJsqfIZOSV5KzcF5Oodxg9DRDy4Cky1Hw1kmnd6dku6xea\nSQ2CGsSPL3kKEYUNGgybvL2nk3BRMLabnSodBDReSwKBgQDlDcT0KYCbAVR0AgCa\nAVjHo8Pwdwm3TZCytpVkVCrgctsq38Fno9A2VTY7OdrEQ91SJKQVjzkcGSgrGg6G\nXF1Ogh8i+tCos8Z7sTouPebVPhpxFzDVfnhru28Hoy2Z8H2CR15DNAAQuaf/C98G\nC8IjMpWIo13nuDXAEH4A9n2gcg==\n-----END PRIVATE KEY-----\n",
-    "client_email": "firebase-adminsdk-t3aln@alamjamaldotme.iam.gserviceaccount.com",
+    "private_key": "",
+    "client_email": "",
     "client_id": "",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-t3aln%40alamjamaldotme.iam.gserviceaccount.com",
+    "auth_uri": "",
+    "token_uri": "",
+    "auth_provider_x509_cert_url": "",
+    "client_x509_cert_url": "",
     "universe_domain": "googleapis.com"
   }
